da7e96cc16d57548096ca67345451f4cdb215b2b
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-05-06 16:52:24 -0300

    lista de produtos disponiveis - responsível

--- dist/brunoklein-JS.js
+++ dist/brunoklein-JS.js
@@ -1,4 +1,7 @@
 $(document).ready(function(){
+    $("#frutasDisponiveis").click(function(){
+        $("#disponivel").stop().slideToggle();
+    });
 	$("#botao").click(function(){
 		var entrada = $("#numero").val();
 		var type = isNaN(entrada); //isNaN -> testa se não é um número
@@ -6,7 +9,7 @@ $(document).ready(function(){
 			$.getJSON("http://192.168.1.109:8080/product?chave="+entrada, function (data){ //.getJSON faz uma requisição e o que retornar ele transforma em JSON
 				var saida = "";
 				saida = "Fruta: " + data.nome + "<br>" +
-				"Valor: " + data.valor + "<br>" +
+				"Valor: R$ " + data.valor + "<br>" +
 				"Status: " + data.status + "<br>" +
 				"Estoque: " + data.estoque + "<br>";
 				$("#dados").html(saida);
@@ -19,7 +22,7 @@ $(document).ready(function(){
 			$.getJSON("http://192.168.1.109:8080/product?nome="+entrada, function (data){
 				var saida = "";
 				saida = "Fruta: " + data.nome + "<br>" +
-				"Valor: " + data.valor + "<br>" +
+				"Valor: R$ " + data.valor + "<br>" +
 				"Status: " + data.status + "<br>" +
 				"Estoque: " + data.estoque + "<br>";
 				$("#dados").html(saida);
@@ -28,5 +31,16 @@ $(document).ready(function(){
 			    $("#dados").html('Fruta não disponível!');
 			})
 		}
-	})
-})
+	});
+	$("#frutasDisponiveis").click(function(){
+		$.getJSON("http://192.168.1.109:8080/list", function (list){
+			var lista = '';
+			var i;
+			for(i=0; i < list.length; i++){
+				lista += list[i].nome +"<br>";
+			}
+			console.log(i);
+			$("#disponivel").html(lista);
+		})
+	});
+});
