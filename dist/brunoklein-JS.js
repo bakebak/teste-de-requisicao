f06da2c1a41748d9d24bf5870331878a37f010fd
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-05-06 14:21:45 -0300

    responsível

--- dist/brunoklein-JS.js
+++ dist/brunoklein-JS.js
@@ -3,9 +3,9 @@ $(document).ready(function(){
 		var entrada = $("#numero").val();
 		var type = isNaN(entrada); //isNaN -> testa se não é um número
 		if(type==false){
-			$.getJSON("http://192.168.1.109:8080/product?chave="+entrada, function (data){
+			$.getJSON("http://192.168.1.109:8080/product?chave="+entrada, function (data){ //.getJSON faz uma requisição e o que retornar ele transforma em JSON
 				var saida = "";
-				saida = "Nome: " + data.nome + "<br>" +
+				saida = "Fruta: " + data.nome + "<br>" +
 				"Valor: " + data.valor + "<br>" +
 				"Status: " + data.status + "<br>" +
 				"Estoque: " + data.estoque + "<br>";
@@ -18,7 +18,7 @@ $(document).ready(function(){
 		else if(type==true){
 			$.getJSON("http://192.168.1.109:8080/product?nome="+entrada, function (data){
 				var saida = "";
-				saida = "Nome: " + data.nome + "<br>" +
+				saida = "Fruta: " + data.nome + "<br>" +
 				"Valor: " + data.valor + "<br>" +
 				"Status: " + data.status + "<br>" +
 				"Estoque: " + data.estoque + "<br>";
