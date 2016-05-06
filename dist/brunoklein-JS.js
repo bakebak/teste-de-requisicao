71534c04039fb49fe27dac8de8802c647fb444fe
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-05-06 10:49:54 -0300

    Avisar sobre frutas não existentes

--- dist/brunoklein-JS.js
+++ dist/brunoklein-JS.js
@@ -1,19 +1,16 @@
 $(document).ready(function(){
-
 	$("#botao").click(function(){
 		var entrada = $("#numero").val();
-		if(entrada!=''){
-			$.getJSON("http://192.168.1.109:8080/product?chave="+entrada, function (data){
-				
-				var saida = "";
-				saida = "Nome: " + data.nome + "<br>" +
-				"Valor: " + data.valor + "<br>" +
-				"Status: " + data.status + "<br>" +
-				"Estoque: " + data.estoque + "<br>";
-				$ ("#dados").html(saida);
-			})
-		}else{
-			$ ("#dados").html(' ');
-		}	
+		$.getJSON("http://192.168.1.109:8080/product?chave="+entrada, function (data){
+			var saida = "";
+			saida = "Nome: " + data.nome + "<br>" +
+			"Valor: " + data.valor + "<br>" +
+			"Status: " + data.status + "<br>" +
+			"Estoque: " + data.estoque + "<br>";
+			$("#dados").html(saida);
+		})
+		.fail(function() {
+		    $("#dados").html('Esse produto não existe!');
+		})
 	})
 })
