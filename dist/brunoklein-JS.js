2f5d7f2e8571e997ce28654ecc523d6efd189a7f
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-05-06 13:06:57 -0300

    Pesquisa por nome e número

--- dist/brunoklein-JS.js
+++ dist/brunoklein-JS.js
@@ -1,16 +1,32 @@
 $(document).ready(function(){
 	$("#botao").click(function(){
 		var entrada = $("#numero").val();
-		$.getJSON("http://192.168.1.109:8080/product?chave="+entrada, function (data){
-			var saida = "";
-			saida = "Nome: " + data.nome + "<br>" +
-			"Valor: " + data.valor + "<br>" +
-			"Status: " + data.status + "<br>" +
-			"Estoque: " + data.estoque + "<br>";
-			$("#dados").html(saida);
-		})
-		.fail(function() {
-		    $("#dados").html('Esse produto não existe!');
-		})
+		var type = isNaN(entrada); //isNaN -> testa se não é um número
+		if(type==false){
+			$.getJSON("http://192.168.1.109:8080/product?chave="+entrada, function (data){
+				var saida = "";
+				saida = "Nome: " + data.nome + "<br>" +
+				"Valor: " + data.valor + "<br>" +
+				"Status: " + data.status + "<br>" +
+				"Estoque: " + data.estoque + "<br>";
+				$("#dados").html(saida);
+			})
+			.fail(function() {
+			    $("#dados").html('Fruta não disponível!');
+			})
+		}
+		else if(type==true){
+			$.getJSON("http://192.168.1.109:8080/product?nome="+entrada, function (data){
+				var saida = "";
+				saida = "Nome: " + data.nome + "<br>" +
+				"Valor: " + data.valor + "<br>" +
+				"Status: " + data.status + "<br>" +
+				"Estoque: " + data.estoque + "<br>";
+				$("#dados").html(saida);
+			})
+			.fail(function() {
+			    $("#dados").html('Fruta não disponível!');
+			})
+		}
 	})
 })
