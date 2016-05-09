130cb7a104c8ee718f9797c407efbfad9e2a1b89
Bruno Augusto Klein <bruno.klein@digitaldesk.com.br>
2016-05-09 11:23:57 -0300

    Lista de frutas com botão de atualizar

--- dist/brunoklein-JS.js
+++ dist/brunoklein-JS.js
@@ -1,46 +1,42 @@
-$(document).ready(function(){
-    $("#frutasDisponiveis").click(function(){
-        $("#disponivel").stop().slideToggle();
-    });
-	$("#botao").click(function(){
-		var entrada = $("#numero").val();
-		var type = isNaN(entrada); //isNaN -> testa se não é um número
-		if(type==false){
-			$.getJSON("http://192.168.1.109:8080/product?chave="+entrada, function (data){ //.getJSON faz uma requisição e o que retornar ele transforma em JSON
-				var saida = "";
-				saida = "Fruta: " + data.nome + "<br>" +
-				"Valor: R$ " + data.valor + "<br>" +
-				"Status: " + data.status + "<br>" +
-				"Estoque: " + data.estoque + "<br>";
-				$("#dados").html(saida);
-			})
-			.fail(function() {
-			    $("#dados").html('Fruta não disponível!');
-			})
-		}
-		else if(type==true){
-			$.getJSON("http://192.168.1.109:8080/product?nome="+entrada, function (data){
-				var saida = "";
-				saida = "Fruta: " + data.nome + "<br>" +
-				"Valor: R$ " + data.valor + "<br>" +
-				"Status: " + data.status + "<br>" +
-				"Estoque: " + data.estoque + "<br>";
-				$("#dados").html(saida);
-			})
-			.fail(function() {
-			    $("#dados").html('Fruta não disponível!');
-			})
-		}
-	});
-	$("#frutasDisponiveis").click(function(){
-		$.getJSON("http://192.168.1.109:8080/list", function (list){
+function chamalista (){
+	$.getJSON("http://192.168.1.109:8080/list", function (list){
 			var lista = '';
 			var i;
 			for(i=0; i < list.length; i++){
-				lista += list[i].nome +"<br>";
+				lista += list[i].chave + " - " + list[i].nome +"<br>";
 			}
 			console.log(i);
 			$("#disponivel").html(lista);
 		})
+}
+
+function chamaindividual(tipo,entrada){
+	$.getJSON("http://192.168.1.109:8080/product?"+tipo+"=" + entrada, function (data){ //.getJSON faz uma requisição e o que retornar ele transforma em JSON
+		var saida = "";
+		saida = "Fruta: " + data.nome + "<br>" +
+		"Valor: R$ " + data.valor + "<br>" +
+		"Status: " + data.status + "<br>" +
+		"Estoque: " + data.estoque + "<br>";
+		$("#dados").html(saida);
+	})
+	.fail(function() {
+	    $("#dados").html('Fruta não disponível!');
+	})
+}
+
+$(document).ready(function(){
+	$("#botao").click(function(){
+		var entrada = $("#numero").val();
+		var type = isNaN(entrada); //isNaN -> testa se não é um número
+		if(type==false){
+			chamaindividual('chave',entrada );
+		}
+		else if(type==true){
+			chamaindividual('nome',entrada);
+		}
+	});
+	chamalista();
+	$("#att").click(function(){
+		chamalista();
 	});
 });
